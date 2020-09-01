5bc63a7b0b112e8d1f455c1167b769c937df0329
Divyam Kumar <divyam.kumar@sap.com>
2020-09-01 17:33:40 +0530

    Hello React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import SayHello from "./sayHello";
+function App(){
+    return(
+        <div className="App">
+            <h1> i am from app component</h1>
+            <SayHello/>
+        </div>
+    );
+}
+
+export default App;
+
---
+++ src/sayHello.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SayHello(){
+    return(
+        <h3>I am from sayHello Component</h3>
+    )
+}
+
+export default SayHello;
